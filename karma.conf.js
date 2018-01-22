e5234afcc4e2bfc946269f10484e56cb87f0be4b
Daniel Santoro <daniel.santoro@amplifieragency.com>
2018-01-23 04:19:01 +1100

    Karma Config

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
     ],
     port: 8080,
     captureTimeout: 60000,
-    frameworks: [ 'mocha', 'chai' ],
+    frameworks: [ 'mocha', 'chai', 'sinon' ],
     client: {
       mocha: {}
     },
